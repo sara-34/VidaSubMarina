d74608f3bba9e23ee1a4c22e5c1e1aa8c99d9014
sara-34 <93350930+sara-34@users.noreply.github.com>
2024-04-03 20:25:56 -0500

    Subiendo maqueta

---
+++ paginaInicio/index.php
@@ -0,0 +1,101 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <nav>
+        <div class="banner">
+            <img src="" alt="Logo">
+            <div>
+               <a href="#">Inicio</a>
+               <a href="#">Contacto</a>
+               <a href="#">Secciones</a>
+            </div>
+        </div>
+    </nav>
+
+    <section class="inicio">
+        <div class="inicio">
+            <img src="" alt="">
+            <p>La vida marina, vida en el mar o vida oceánica, 
+            la conforman las plantas, los animales y otros organismos 
+            que viven en el agua salada de los mares y océanos, 
+            o el agua salobre de los estuarios costeros. 
+            En un nivel fundamental, la vida marina ayuda a 
+            determinar la naturaleza misma de nuestro planeta.</p>
+        </div>
+    </section>
+
+    <section class="cards">
+        <div class="btn">
+            <img src="" alt="">
+            <button>Ver mas</button>
+        </div>
+
+        <div class="cards">
+            <div class="card">
+                <img src="" alt="">
+                <p>La vida marina, vida en el mar o vida oceánica, 
+                la conforman las plantas, los animales y otros organismos 
+                que viven en el agua salada de los mares y océanos, 
+                o el agua salobre de los estuarios costeros. 
+                En un nivel fundamental, la vida marina ayuda a 
+                determinar la naturaleza misma de nuestro planeta.
+                </p>
+            </div>
+
+            <div class="card">
+                <img src="" alt="">
+                <p>La vida marina, vida en el mar o vida oceánica, 
+                la conforman las plantas, los animales y otros organismos 
+                que viven en el agua salada de los mares y océanos, 
+                o el agua salobre de los estuarios costeros. 
+                En un nivel fundamental, la vida marina ayuda a 
+                determinar la naturaleza misma de nuestro planeta.
+                </p>
+            </div>
+
+            <div class="card">
+                <img src="" alt="">
+                <p>La vida marina, vida en el mar o vida oceánica, 
+                la conforman las plantas, los animales y otros organismos 
+                que viven en el agua salada de los mares y océanos, 
+                o el agua salobre de los estuarios costeros. 
+                En un nivel fundamental, la vida marina ayuda a 
+                determinar la naturaleza misma de nuestro planeta.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="noticias">
+        <div class="noticias">
+            <img src="" alt="">
+            <p></p>
+        </div>
+
+        <div class="boton">
+            <img src="" alt="">
+            <button>Ver mas</button>
+        </div>
+    </section>
+
+    <section class="infomacion">
+        <div class="Informacion">
+
+        </div>
+
+        <div class="comentarios">
+            <input type="text" name="" id="">
+            <input type="button" value="submit">
+        </div>
+    </section>
+</body>
+</html>
