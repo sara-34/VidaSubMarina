f98e7ebc3885911783e6eff1cdff1a7cd2001a64
HAROL V <harolvelez32@gmail.com>
2024-04-12 10:39:55 -0500

    ultimos cambios de las secciones

--- paginaInicio/paginasecciones/seccion5.php
+++ paginaInicio/paginasecciones/seccion5.php
@@ -3,9 +3,377 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>seccion1</title>
+    <title>seccion5</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="seccion1.css">
+
 </head>
 <body>
-    <h1>hola5</h1>
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a href="#" class="navbar-brand"> <span class="text-primary">Vida</span>Submarina</a>
+        <button class="navbar-toggler" type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarS"
+            aria-controls="navbarS"
+            aria-expanded="false"
+            aria-label="toggle navigation"
+        >
+        <span class="navbar-toggle-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbars">
+            <ul class="navbar-nav ms-auto mb-2 mb.lg-0">
+                <li class="nav-item">
+                    <a href="/VidaSubmarina/paginaInicio/" class="nav-link">inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#footer" class="nav-link">contactanos</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/VidaSubmarina/paginaInicio/paginasecciones/secciones.php" class="nav-link">secciones</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
+<div id="carouselE" class="carousel slide" data-bs-ride="carousel">
+    
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselE" data-bs-slide-to="0" class="active" aria-current="true"
+            aria-label="Slide 1"></button>
+
+        <button type="button" data-bs-target="#carouselE" data-bs-slide-to="1" class="active" aria-current="true"
+            aria-label="Slide 2"></button>
+
+         <button type="button" data-bs-target="#carouselE" data-bs-slide-to="2" class="active" aria-current="true"
+            aria-label="Slide 3"></button>
+    </div>
+
+    <div class="carousel-inner">
+        
+        <div class="carousel-item active">
+            <img src="../imagenes/img8.jpg" class="d-block w-100" alt="1">
+            <div class="carousel-caption">
+                <h5>ANIMALES EXTINTOS</h5>
+                <p>
+                    Lorem ipsum dolor,sit amet consectetur adipisicing elit. Odio eaque harum 
+                    natus aliquid pariatur quas mollitia et ipsam inventore blanditiis quam ratione, alias cumque quo 
+                    odit magni. Ullam, atque iure?
+                </p>
+            </div>
+        </div>
+
+
+        <div class="carousel-item ">
+            <img src="../imagenes/img9.jpg" class="d-block w-100" alt="1">
+            <div class="carousel-caption">
+                <h5>eeeeee</h5>
+                <p>
+                    Lorem ipsum dolor,sit amet consectetur adipisicing elit. Odio eaque harum 
+                    natus aliquid pariatur quas mollitia et ipsam inventore blanditiis quam ratione, alias cumque quo 
+                    odit magni. Ullam, atque iure?
+                </p>
+            </div>
+        </div>
+
+
+
+        <div class="carousel-item ">
+            <img src="../imagenes/img10.jpg" class="d-block w-100" alt="1">
+            <div class="carousel-caption">
+                <h5>aaaaaaaa</h5>
+                <p>
+                    Lorem ipsum dolor,sit amet consectetur adipisicing elit. Odio eaque harum 
+                    natus aliquid pariatur quas mollitia et ipsam inventore blanditiis quam ratione, alias cumque quo 
+                    odit magni. Ullam, atque iure?
+                </p>
+            </div>
+        </div>
+
+
+    </div>
+
+        <button class="carousel-control-prev" type="button"
+               data-bs-target="#carouselE"
+               data-bs-slide="prev"
+        
+        >
+
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">previous</span>
+
+        </button>
+
+        <button class="carousel-control-next" type="button"
+               data-bs-target="#carouselE"
+               data-bs-slide="next"
+        
+        >
+
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">next</span>
+
+        </button>
+    
+</div>
+
+
+<section class="about section-padding">
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-12 col-12">
+            <div class="about-img">
+                <img src="../imagenes/img11.jpg" alt="">
+            </div>
+        </div>
+        <div class="col-lg-8 col-md-12 col-12 ps-lg-5 mt-md-5">
+            <div class="about-text text-white">
+                <h2>Introduccion a la vida marina <br>SABIAS QUE</h2>
+                <P>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quo odit eligendi
+                    quaerat quae voluptatibus facilis ab cum pariatur magnam,
+                    sunt fugiat ipsa officia laudantium tenetur quibusdam porro deleniti? Placeat!
+                </P>
+                <a href="#" class="btn btn-primary">mas informacion</a>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-4 col-md-12 col-12">
+            <div class="about-img">
+                <img src="../imagenes/img12.jpg" alt="">
+            </div>
+        </div>
+        <div class="col-lg-8 col-md-12 col-12 ps-lg-5 mt-md-5">
+            <div class="about-text text-white">
+                <h2>Introduccion a la vida marina <br>SABIAS QUE</h2>
+                <P>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quo odit eligendi
+                    quaerat quae voluptatibus facilis ab cum pariatur magnam,
+                    sunt fugiat ipsa officia laudantium tenetur quibusdam porro deleniti? Placeat!
+                </P>
+                <a href="#" class="btn btn-primary">mas informacion</a>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-4 col-md-12 col-12">
+            <div class="about-img">
+                <img src="../imagenes/img13.jpg" alt="">
+            </div>
+        </div>
+        <div class="col-lg-8 col-md-12 col-12 ps-lg-5 mt-md-5">
+            <div class="about-text text-white">
+                <h2>Introduccion a la vida marina <br>SABIAS QUE</h2>
+                <P>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quo odit eligendi
+                    quaerat quae voluptatibus facilis ab cum pariatur magnam,
+                    sunt fugiat ipsa officia laudantium tenetur quibusdam porro deleniti? Placeat!
+                </P>
+                <a href="#" class="btn btn-primary">mas informacion</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</section>
+
+<section class="services section-padding">
+    
+<div class="container">
+
+    <div class="row">
+        <div class="col-md.12">
+            <div class="section-header text-center text-white pb-5">
+                <h2>ANIMALES QUE PELIGRAN</h2>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Animi numquam magnam explicabo architecto maxime officia illum, 
+                    minus, placeat pariatur voluptate quod. Pariatur, dignissimos sequi. Nobis commodi reprehenderit
+                    architecto veniam molestias?
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+
+        <div class="col-12 col-md-12 col-lg-4">
+            <div class="card text-white text-center bg-dark pb-2">
+                <div class="card-body">
+                    <i class="bi bi-laptop"></i>
+                    <h3 class="card-tittle">registro</h3>
+                    <p class="lead">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam 
+                        tenetur quod harum reiciendis numquam tempore velit temporibus
+                        minima veniam inventore vero similique cum, modi odio ad quisquam. Sed, molestias in?
+                    </p>
+                    <button class="btn bg-primary text-white">mas informacion</button>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-12 col-md-12 col-lg-4">
+            <div class="card text-white text-center bg-dark pb-2">
+                <div class="card-body">
+                    <i class="bi bi-wifi"></i>
+                    <h3 class="card-tittle">internet</h3>
+                    <p class="lead">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam 
+                        tenetur quod harum reiciendis numquam tempore velit temporibus
+                        minima veniam inventore vero similique cum, modi odio ad quisquam. Sed, molestias in?
+                    </p>
+                    <button class="btn bg-primary text-white">mas informacion</button>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-12 col-md-12 col-lg-4">
+            <div class="card text-white text-center bg-dark pb-2">
+                <div class="card-body">
+                    <i class="bi bi-tv"></i>
+                    <h3 class="card-tittle">tv</h3>
+                    <p class="lead">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam 
+                        tenetur quod harum reiciendis numquam tempore velit temporibus
+                        minima veniam inventore vero similique cum, modi odio ad quisquam. Sed, molestias in?
+                    </p>
+                    <button class="btn bg-primary text-white">mas informacion</button>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+
+</section>
+
+
+
+<section class="portafolio section-padding">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="section-header text-center text-white pb-5">
+                <h2>sabias que</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, 
+                    atque. Adipisci aut consequuntur aliquid quaerat, veritatis rerum atque 
+                    non earum natus minima unde fugit explicabo maxime illo nihil sunt corporis.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-md-12 col-lg-4">
+            <div class="card text-light text-center bg-dark pb-2">
+                <div class="card-body text-white">
+                    <div class="img-area mb-4">
+                        <img src="../imagenes/img14.jpg" alt="">
+                    </div>
+                    <h3>mira esto 1</h3>
+                    <p class="lead">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro perferendis amet voluptatum. Temporibus dolore provident eligendi, tempore consectetur mollitia veniam dicta explicabo expedita tenetur aut fugit perspiciatis veritatis, maxime doloremque?
+                    </p>
+                    <button class="btn bg-primary text-white">mas informacion</button>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-12 col-md-12 col-lg-4">
+            <div class="card text-light text-center bg-dark pb-2">
+                <div class="card-body text-white">
+                    <div class="img-area mb-4">
+                        <img src="../imagenes/img15.jpg" alt="">
+                    </div>
+                    <h3>mira esto 2</h3>
+                    <p class="lead">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro perferendis amet voluptatum. Temporibus dolore provident eligendi, tempore consectetur mollitia veniam dicta explicabo expedita tenetur aut fugit perspiciatis veritatis, maxime doloremque?
+                    </p>
+                    <button class="btn bg-primary text-white">mas informacion</button>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-12 col-md-12 col-lg-4">
+            <div class="card text-light text-center bg-dark pb-2">
+                <div class="card-body text-white">
+                    <div class="img-area mb-4">
+                        <img src="../imagenes/img1.jpg" alt="">
+                    </div>
+                    <h3>mira esto 3</h3>
+                    <p class="lead">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro perferendis amet voluptatum. Temporibus dolore provident eligendi, tempore consectetur mollitia veniam dicta explicabo expedita tenetur aut fugit perspiciatis veritatis, maxime doloremque?
+                    </p>
+                    <button class="btn bg-primary text-white">mas informacion</button>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+
+
+
+</section>
+
+
+<section id="section-4">
+     <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-2.45,22.22 C120.49,94.25 336.05,2.48 502.45,45.89 L500.00,150.00 L-3.28,151.48 Z" style="stroke: none; fill: rgb(255, 255, 255);"></path></svg></div>
+     </section>
+
+     <footer id="footer">
+      <div class="cajaInfo">
+      <div class="contacto">
+    <h4><i class="fas fa-envelope"></i> Correo Electronico</h4>
+    <p>vidamarina@gmail.com</p><br>
+
+    <h4><i class="fas fa-phone"></i> Telefono</h4>
+    <p>3134567889</p><br>
+
+    <h4><i class="fas fa-map-marker-alt"></i> Dirección</h4>
+    <p>Carrera 92#67 Ibague</p>
+</div>
+
+
+        <div class="cajaComentario">
+            <form action="" method="post">
+                <h5>Ingresa tu comentario de recomendacíon:</h5>
+                <textarea rows="5" cols="30"></textarea><br><br>
+               <input type="submit" value="Enviar comentario">
+            </form>
+
+        </div>
+
+     </div>
+        
+     </footer>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
